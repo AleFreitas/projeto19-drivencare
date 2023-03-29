2457b4b8c23fdb1736c6b0a47738fbea9416633c
Alexandre <alexandrefsoficial@gmail.com>
2023-03-29 11:16:03 -0300

    joi validation middleware implemented

---
+++ src/middlewares/validateSchema.js
@@ -0,0 +1,13 @@
+export function validateSchema(schema) {
+    return (req, res, next) => {
+        const validate = schema.validate(req.body, { abortEarly: false })
+        if (validate.error) {
+            const erros = validate.error.details.map((err) => {
+                return err.message
+            })
+            return res.status(422).send(erros)
+        }
+        next()
+
+    }
+}
