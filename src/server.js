d546517aa12352dfcc67023d028a0054d58c1c16
Alexandre <alexandrefsoficial@gmail.com>
2023-03-29 11:10:33 -0300

    server added

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import postsRouter from "./routes/postRoutes.js";
+import userAccessRouter from "./routes/userAccessRoutes.js";
+import hashtagsRoutes from "./routes/hashtagsRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use([userRoutes, postsRouter, userAccessRouter, hashtagsRoutes]);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Servidor funfando na porta: ${port}`));
